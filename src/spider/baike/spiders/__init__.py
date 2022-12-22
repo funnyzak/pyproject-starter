e0dd07999023f8fcefb9ee6781db2c4f82c4e12f
leon <silenceace@gmail.com>
2022-12-22 16:32:25 +0800

    feat: create baike spider

---
+++ src/spider/baike/spiders/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
